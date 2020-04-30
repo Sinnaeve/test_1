54667c5aeb75fdf8a3061adbaff2e9bdcb9f9d5f
Sinnaeve <adrien.sinnaeve@gmail.com>
2020-04-30 14:38:01 +0200

    model_fn add in an inference.py file

---
+++ inference.py
@@ -0,0 +1,13 @@
+import os
+import torch
+
+from modeles_library import my_lstm
+
+
+def model_fn(model_dir):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = my_lstm(embedding_dim=1, hidden_dim=1, target_size=1, num_layers=1)
+    print("model:", model)
+    with open(os.path.join(model_dir, 'model.pth'), 'rb') as f:
+        model.load_state_dict(torch.load(f))
+    return model.to(device)
