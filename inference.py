bc97621be2afc3fabeb1cc1abeda10dd006a5da2
adrien <adrisin4@gmail.com>
2020-05-01 16:59:35 +0000

    test push studio public

--- inference.py
+++ inference.py
@@ -11,3 +11,5 @@ def model_fn(model_dir):
     with open(os.path.join(model_dir, 'model.pth'), 'rb') as f:
         model.load_state_dict(torch.load(f))
     return model.to(device)
+
+print("studio push succes")
